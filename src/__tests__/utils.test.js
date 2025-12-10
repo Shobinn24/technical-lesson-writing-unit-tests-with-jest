6c7da801d06171a12cb933b539cde666536a87c8
shobinn24 <shobinn24@gmail.com>
2025-12-10 10:30:33 -0500

    unittest

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,27 @@
-// Your tests here
+import { pointsForWord } from "../utils";
+
+describe("pointsForWord", () => {
+  it("calculates the total points for a word (1 point per vowel, 2 per consonant)", () => {
+    const word = "test";
+    const points = pointsForWord(word);
+    expect(points).toBe(7);
+  });
+
+  it("handles uppercase and lowercase input", () => {
+    const word = "TeSt";
+    const points = pointsForWord(word);
+    expect(points).toBe(7);
+  });
+
+  it("returns 0 for an empty string", () => {
+    const word = "";
+    const points = pointsForWord(word);
+    expect(points).toBe(0);
+  });
+
+  it("ignores non-alphanumeric characters", () => {
+    const word = "hello!";
+    const points = pointsForWord(word);
+    expect(points).toBe(8);
+  });
+});
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,8 @@
-// Your code here
+export function pointsForWord(word) {
+  return [...word].reduce((points, char) => {
+    if (/[a-z]/i.test(char)) { 
+        return points + (/[aeiou]/i.test(char) ? 1 :2);
+    }
+    return points;
+  }, 0);
+}
